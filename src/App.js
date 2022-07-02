7e0fb3c4484227ee7b49d2442c8a341d79c533ef
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-02 18:40:10 +0400

    Introduce useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -7,12 +7,12 @@ import './App.css';
 function App() {
   const [showParagraph, setShowParagraph] = useState();
 
-  const togglreParagraphHandler = () => {
+  const togglreParagraphHandler = useCallback(() => {
     setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  };
+  }, []);
 
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
       <DemoOutput show={false} />
       <Button onClick={togglreParagraphHandler}>Show Paragraph</Button>
