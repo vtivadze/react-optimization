7096bd1658e8fab6659a9fc54cc2ce21520dd71a
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-02 17:59:43 +0400

    DemonstrateReact components in-vain execution and rendering example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState();
+
+  const togglreParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={togglreParagraphHandler}>Show Paragraph</Button>
     </div>
   );
 }
